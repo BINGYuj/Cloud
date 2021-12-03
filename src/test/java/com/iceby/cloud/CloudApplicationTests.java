7e06ccec45e971b335553123400210f871d7abe3
dengyuping <510528509@qq.com>
2021-12-03 18:54:11 +0800

    初始化项目

---
+++ src/test/java/com/iceby/cloud/CloudApplicationTests.java
@@ -0,0 +1,42 @@
+package com.iceby.cloud;
+
+import com.iceby.cloud.web.controller.HelloController;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockRequestDispatcher;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class CloudApplicationTests {
+
+    private MockMvc mvc;
+
+    @BeforeAll
+    public void setUp() throws Exception{
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+}
